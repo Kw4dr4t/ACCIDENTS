ea3075a301c501cabebfad2e2496b30a309db507
Michał Małek <michal.malek1@gmail.com>
2020-11-27 12:48:12 +0100

    update

--- accident_data.py
+++ accident_data.py
@@ -2,9 +2,8 @@ import pandas as pd
 import folium
 from folium.plugins import HeatMapWithTime
 
-
 df1 = pd.read_csv('traffic_accidents_2014.csv', header=0)
-#df1.columns = column_names
+# df1.columns = column_names
 print(df1)
 df1.shape
 df1.head()
@@ -25,10 +24,11 @@ df1 = df1.dropna(subset=['Latitude', 'Longitude'])
 df1.shape
 df2.shape
 print(df1.loc[(df1['num_of_fatalities'] >= 1.0), [
-      'district_commune', 'Latitude', 'Longitude', 'num_of_fatalities']])
+    'district_commune', 'Latitude', 'Longitude', 'num_of_fatalities']])
 df3 = df1[df1['num_of_fatalities'] < 0]
 df3.shape
 from folium.plugins import HeatMap
+
 df_map = df1.copy()
 df_map['count'] = 1
 df_map[['Latitude', 'Longitude', 'count']].groupby(
@@ -38,10 +38,9 @@ df_map.shape
 print(df1['type_of_injury'].unique())
 
 
-def generateBaseMap(default_location=[51.519120, 21.281183], default_zoom_start=12):
-    base_map = folium.Map(location=default_location,
-                          control_scale=True, zoom_start=default_zoom_start)
-    return base_map
+def generateBaseMap(default_location: object = [51.519120, 21.281183], default_zoom_start: object = 12) -> object:
+    return folium.Map(location=default_location,
+                      control_scale=True, zoom_start=default_zoom_start)
 
 
 base_map = generateBaseMap()
@@ -59,21 +58,22 @@ df_map[['weather_conditions', 'date', 'count']].groupby(
 print('hello')
 print(df2.head)
 
-
-
-df2['count']=1
+df2['count'] = 1
 df5 = df2[['district_commune', 'count']].groupby(['district_commune']).sum().sort_values('count', ascending=False)
 df5.shape
 df5.head
 pd.DataFrame(df5).to_csv('/home/michalm/Projects/Python/ACCIDENTS/towns.csv')
+
 
 def f(var):
     if isinstance(var, pd.DataFrame):
         print('dataframe')
     else:
         print('not a dataframe, prob a list')
+
+
 f(df5)
-town_names=[]
+town_names = []
 df2.dropna(subset=['district_commune'])
 town_names = df2['district_commune'].unique()
 print(town_names)
