3cd2b68945cd9518e2e94543873894a392fe4ccd
michalm <michal.malek1@gmail.com>
2020-11-30 23:08:38 +0100

    pep8 changes

--- accident_data.py
+++ accident_data.py
@@ -25,20 +25,23 @@ df3 = df1[df1['num_of_fatalities'] < 0]
 df_map = df1.copy()
 df_map['count'] = 1
 df_map[['Latitude', 'Longitude', 'count']].groupby(
-    ['Latitude', 'Longitude']).sum().sort_values('count', ascending=False).head(10)
+    ['Latitude', 'Longitude']).sum().sort_values(
+        'count', ascending=False).head(10)
 
 print(df1['type_of_injury'].unique())
 
 
-def generate_base_map(default_location: object = [51.519120, 21.281183], default_zoom_start: object = 12) -> object:
+def generate_base_map(default_location: object = [51.519120, 21.281183],
+                      default_zoom_start: object = 12) -> object:
     return folium.Map(location=default_location,
                       control_scale=True, zoom_start=default_zoom_start)
 
 
 base_map = generate_base_map()
 m = HeatMap(data=df_map[['Latitude', 'Longitude', 'count']].groupby(
-    ['Latitude', 'Longitude']).sum().reset_index().values.tolist(), radius=7, max_zoom=10).add_to(base_map)
-m.save('/home/michalm/Projects/Python/ACCIDENTS/heatmap.html')
+    ['Latitude', 'Longitude']).sum().reset_index().values.tolist(), radius=7,
+            max_zoom=10).add_to(base_map)
+m.save('heatmap.html')
 
 df_map.head()
 
@@ -50,8 +53,9 @@ print('hello')
 print(df2.head(3))
 
 df2['count'] = 0
-df5 = df2[['district_commune', 'count']].groupby(['district_commune']).sum().sort_values('count', ascending=False)
-pd.DataFrame(df5).to_csv('/home/michalm/Projects/Python/ACCIDENTS/towns.csv')
+df5 = df2[['district_commune', 'count']].groupby(
+    ['district_commune']).sum().sort_values('count', ascending=False)
+pd.DataFrame(df5).to_csv('towns.csv')
 
 
 def f(var):
