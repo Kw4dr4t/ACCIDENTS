b7280c778fc0a683d13ea03c3dee749eb966ad7c
michal <michal.malek1@gmail.com>
2020-12-21 15:46:45 +0100

    update

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-import pandas as pd
-
-df1 = pd.read_csv('traffic_accidents_2014.csv', header=0)
-print(df1)
-df1.shape
-df1.head()
-# convert date for pandas
-df1['date'] = pd.to_datetime(df1["date"], format = '%Y-%m-%d')
-print(df1.dtypes)
-df1.head()
-print(df1['date'])
-
-print(df1['Latitude'])
-print(df1.dtypes)
+folium
